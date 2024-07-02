1393b4df7c9b36fc2eeb33e9972bc49ba90ff7bf
BerkAkin <berkakin99@hotmail.com>
2024-07-02 13:55:42 +0300

    practised variables and operations

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -1,4 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int a = 32;
-Console.WriteLine(a);
+﻿Console.WriteLine("Hello, World!");
+
+
+
+//Değişkenler ve İşlemler
+
+string firstName;
+firstName ="Berk";
+Console.WriteLine(firstName);
+
+var Random = "random";
+Console.WriteLine(Random);
+
+string name= "Bob";
+int messageCount= 3;
+float temp = 34.4F;
+
+
+Console.WriteLine("Hello, " + name + "! You have " + messageCount + " in your inbox. The temperature is " + temp + " celcius");
+Console.WriteLine(34.40M);
+
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
--- Lessons/obj/Debug/net8.0/Lessons.AssemblyInfo.cs
+++ Lessons/obj/Debug/net8.0/Lessons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67043914089219834323aa6f37c38aebd9a5fd94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
