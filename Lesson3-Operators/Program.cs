08cce506ace593429a2ec80fd5776c5370772ddb
BerkAkin <berkakin99@hotmail.com>
2024-07-04 23:37:23 +0300

    practised operators

--- Lesson3-Operators/Program.cs
+++ Lesson3-Operators/Program.cs
@@ -6,7 +6,38 @@ namespace Lesson3_Operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Atama ve işlemli atama
+
+            int x = 1;
+            int y = 2;
+            y = y+2 ;
+            y +=2 ;
+            y /= 1;
+            x *= 2;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            //Mantıksal Operatörler
+            // || && 
+
+            bool isSucces = false;
+            bool isCompleted = true;
+
+            //İlişkisel Operatörler
+
+            if(3<2){
+                Console.WriteLine("");
+            }
+            if(3>2){
+                Console.WriteLine("");
+            }
+            if(3==2){
+                Console.WriteLine("");
+            }
+            if(3!=2){
+                Console.WriteLine("");
+            }
+
         }
     }
 }
