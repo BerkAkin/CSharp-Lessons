de676a6733fc9b31ffe0745cf6a1979d809fb2f4
BerkAkin <berkakin99@hotmail.com>
2024-07-04 23:39:41 +0300

    initial commit for lesson4

---
+++ Lesson4-TypeCasting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson4_TypeCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
