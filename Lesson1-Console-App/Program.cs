de26ea88d2727e45a5e1b2f82763371f7adea405
BerkAkin <berkakin99@hotmail.com>
2024-07-04 16:44:30 +0300

    initial commit

---
+++ Lesson1-Console-App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name= Console.ReadLine();
+            Console.WriteLine("Merhaba: "+name);
+        }
+    }
+}
