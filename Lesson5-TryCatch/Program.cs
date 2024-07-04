e92f76000fcbf22c6e8c8935417d0f285933d0a9
BerkAkin <berkakin99@hotmail.com>
2024-07-05 00:55:03 +0300

    practised try catch and error handling via exceptions

--- Lesson5-TryCatch/Program.cs
+++ Lesson5-TryCatch/Program.cs
@@ -6,7 +6,48 @@ namespace Lesson5_TryCatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Sayı gir");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("sayi:" + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Program Sonlandı");
+            }
+
+
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("left");
+                int a = int.Parse("-230428912589");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri Tipi Uygun Değil");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Değer, değişkenin alabileceği uzunluğun dışında");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Program Sonlandı");
+            }
         }
     }
 }
