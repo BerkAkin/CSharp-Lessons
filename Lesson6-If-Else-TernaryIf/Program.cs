35f58f299fe1efd31c59a0a4a79aab5697573244
BerkAkin <berkakin99@hotmail.com>
2024-07-05 01:09:00 +0300

    initial commit for lesson6

---
+++ Lesson6-If-Else-TernaryIf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson6_If_Else_TernaryIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
