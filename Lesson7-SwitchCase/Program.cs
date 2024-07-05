a9a3eea51098afd5455f5c08654e63c918d1971d
BerkAkin <berkakin99@hotmail.com>
2024-07-05 15:33:39 +0300

    practised switch case

---
+++ Lesson7-SwitchCase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson7_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1: Console.WriteLine("1"); break;
+                case 2: Console.WriteLine("2"); break;
+                default: Console.WriteLine("default"); break;
+            }
+
+        }
+    }
+}
