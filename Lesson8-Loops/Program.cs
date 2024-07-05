57479f90a8dfcf5c8041919f8fedc817d0012824
BerkAkin <berkakin99@hotmail.com>
2024-07-05 15:42:12 +0300

    practised loops

--- Lesson8-Loops/Program.cs
+++ Lesson8-Loops/Program.cs
@@ -6,7 +6,42 @@ namespace Lesson8_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 2)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (i == 4)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            int b = 0;
+            while (b < 12)
+            {
+                Console.WriteLine(b);
+                b++;
+            }
+
+            do
+            {
+                Console.WriteLine(b);
+                Console.WriteLine("bir kez çalıştı");
+                b++;
+            } while (b < 0);
+
+            int[] dizi = new int[] { 1, 2, 3, 4, 45, 5, 7, 8 };
+            foreach (int eleman in dizi)
+            {
+                Console.WriteLine(eleman);
+            }
         }
     }
 }
