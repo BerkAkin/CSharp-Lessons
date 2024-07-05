c97634abe3f1293a277399980708a94c1672b2b6
BerkAkin <berkakin99@hotmail.com>
2024-07-05 18:15:14 +0300

    practised methods

--- Lesson10-Functions/Program.cs
+++ Lesson10-Functions/Program.cs
@@ -6,7 +6,33 @@ namespace Lesson10_Functions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 4;
+            int b = 2;
+            Metotlar ornek = new Metotlar();
+            ornek.Yaz(Convert.ToString(Topla(a, b)));
+
+            ornek.Yaz(Convert.ToString(ornek.ArttırVeTopla(ref a, ref b)));
+        }
+        public static int Topla(int a, int b)
+        {
+            return a + b;
+        }
+
+
+    }
+
+    class Metotlar
+    {
+        public void Yaz(string veri)
+        {
+            Console.WriteLine($"Yaz: {veri}");
+        }
+
+        public int ArttırVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
         }
     }
 }
