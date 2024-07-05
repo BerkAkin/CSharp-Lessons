6db2dc4f7587968be29db12fc2611111f22ec7e2
BerkAkin <berkakin99@hotmail.com>
2024-07-05 17:54:09 +0300

    initial commit for function lessons

---
+++ Lesson10-Functions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson10_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
