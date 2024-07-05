010e6942f4e3492413e90b84dd4dff20c0fa66cc
BerkAkin <berkakin99@hotmail.com>
2024-07-05 04:14:12 +0300

    practised if blocks

--- Lesson6-If-Else-TernaryIf/Program.cs
+++ Lesson6-If-Else-TernaryIf/Program.cs
@@ -6,7 +6,24 @@ namespace Lesson6_If_Else_TernaryIf
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //If Blokları
+            int a = 5;
+            if (a > 5)
+            {
+                Console.WriteLine("A  5'ten büyük");
+            }
+            else if (a < 5)
+            {
+                Console.WriteLine("A  5'ten küçük");
+            }
+            else
+            {
+                Console.WriteLine("A  5'e eşit");
+            }
+
+            //Ternary If
+            bool bool1 = a < 5 ? true : false;
+
         }
     }
 }
