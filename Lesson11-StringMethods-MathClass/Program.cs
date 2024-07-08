c4926f02535a9333d6c0f95f2d0830589b66368a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-08 03:44:00 +0300

    practised String Methods

--- Lesson11-StringMethods-MathClass/Program.cs
+++ Lesson11-StringMethods-MathClass/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lesson11_StringMethods_MathClass
 {
@@ -6,7 +7,24 @@ namespace Lesson11_StringMethods_MathClass
     {
         static void Main(string[] args)
         {
-            C
+            string kelime = "Kelime";
+
+            Console.WriteLine(kelime.ToLower());
+            Console.WriteLine(kelime.ToUpper());
+            Console.WriteLine(kelime.Length);
+            Console.WriteLine(String.Concat(kelime, " Concatted"));
+            Console.WriteLine(kelime.CompareTo("Keli"));
+            Console.WriteLine(String.Compare(kelime, "Keli"));
+            Console.WriteLine(kelime.Insert(5, "5.INDEX"));
+            Console.WriteLine(kelime.Substring(5));
+            Console.WriteLine(kelime.EndsWith("d"));
+            Console.WriteLine(kelime.StartsWith("K"));
+            Console.WriteLine(kelime.IndexOf("e"));
+            Console.WriteLine(kelime.LastIndexOf("e"));
+            Console.WriteLine(kelime.PadLeft(50, 'a'));
+            Console.WriteLine(kelime.PadRight(50, 'a'));
+            Console.WriteLine(kelime.Remove(5));
+            Console.WriteLine(kelime.Split("")[0]);
         }
     }
 }
