c00d7ad51bbe551e754f6da4f09764591b3f810b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-14 05:15:20 +0300

    initial commit

---
+++ Lesson14-StaticClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson14_StaticClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
