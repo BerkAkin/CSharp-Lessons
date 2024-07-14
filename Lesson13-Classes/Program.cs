21704b259f17528a26e1accff80e21c226dce02c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-14 04:03:48 +0300

    practsied classes

--- Lesson13-Classes/Program.cs
+++ Lesson13-Classes/Program.cs
@@ -6,7 +6,27 @@ namespace Lesson13_Classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calisan calisan = new Calisan();
+            calisan.Ad = "Berk";
+            calisan.Soyad = "Akın";
+            calisan.Departman = "Yazılım";
+            calisan.No = 41266212;
+
+            calisan.getBilgi();
+
         }
     }
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void getBilgi()
+        {
+            Console.WriteLine("Çalışan Bilgileri: " + this.Ad + ", " + this.Soyad + ", " + this.No + ", " + this.Departman);
+        }
+    }
+
 }
