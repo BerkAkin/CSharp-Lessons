edc42ed947f562112cb95fa11e2ec53f154f031a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-17 04:09:31 +0300

    initial commit

---
+++ Lesson17-Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson17_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
