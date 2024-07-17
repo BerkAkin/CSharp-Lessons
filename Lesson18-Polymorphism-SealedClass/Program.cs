4ecdee23332c380450af8c82f9b0eb25977323b8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-17 17:14:57 +0300

    inital commit for lesson 18

---
+++ Lesson18-Polymorphism-SealedClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson18_Polymorphism_SealedClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
