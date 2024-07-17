adedb8fd444ba0f0a369d4c11f04748c8ed7b4ab
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-17 19:14:53 +0300

    initial commit for abstract classes

---
+++ Lesson20-AbstractClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson20_AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
