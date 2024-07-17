7a2ef0a7521af7bd56f2f9cb95dcb0dd186f03ba
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-17 23:40:03 +0300

    intial commit for todo console app

---
+++ Prıject2-ToDoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prıject2_ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
