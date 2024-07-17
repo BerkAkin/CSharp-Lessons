d368202debb8e0d286f378e7b93b72fd1149520c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-17 03:48:48 +0300

    initial commit

---
+++ Lesson16-Enum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson16_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
