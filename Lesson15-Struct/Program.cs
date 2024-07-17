043fcdf7a412ad4290a936ace7f8a24fcedbaabe
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-17 03:42:30 +0300

    practised structs

--- Lesson15-Struct/Program.cs
+++ Lesson15-Struct/Program.cs
@@ -6,7 +6,42 @@ namespace Lesson15_Struct
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.kisaKenar = 3;
+            dikdortgen.uzunKenar = 4;
+            Console.WriteLine("Class Alan: " + dikdortgen.AlanHesapla());
+
+
+            DikdortgenStruct dikdortgen1 = new DikdortgenStruct(3, 5);
+            Console.WriteLine("Struct Alan: " + dikdortgen1.AlanHesapla());
         }
+
+    }
+
+
+    class Dikdortgen
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+    }
+
+    public struct DikdortgenStruct
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+        public long AlanHesapla()
+        {
+            return this.uzunKenar * this.kisaKenar;
+        }
+        public DikdortgenStruct(int kisaKenar, int uzunKenar)
+        {
+            this.kisaKenar = kisaKenar;
+            this.uzunKenar = uzunKenar;
+        }
+
     }
 }
