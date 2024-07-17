38f22afe5cbc7264573c97679af5c18c3dbb7d92
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-17 04:03:10 +0300

    practised enums

--- Lesson16-Enum/Program.cs
+++ Lesson16-Enum/Program.cs
@@ -6,7 +6,43 @@ namespace Lesson16_Enum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Gunler.Salı);
+            Console.WriteLine((int)Gunler.Pazartesi);
+
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Hava Soğuk");
+            }
+
+
+
         }
+
+    }
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+
+    class Araba
+    {
+        static Araba() { }
+        public Araba() { }
     }
 }
