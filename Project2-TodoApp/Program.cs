a4728cc206d72d640c07f5764c1afe3cc22849cf
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-18 23:53:26 +0300

    default definitons are now in operations file

--- Project2-TodoApp/Program.cs
+++ Project2-TodoApp/Program.cs
@@ -8,20 +8,15 @@ namespace Project2_TodoApp
     {
         static void Main(string[] args)
         {
-            Card gorev1 = new Card("gorev1", "gorev1 aciklama", Sizes.S.ToString(), KisiListesi.BerkAkin);
-            Card gorev2 = new Card("gorev2", "gorev2 aciklama", Sizes.M.ToString(), KisiListesi.CemSeckin);
-            Card gorev3 = new Card("gorev3", "gorev3 aciklama", Sizes.XL.ToString(), KisiListesi.SemihaAy);
-
-            //Varsayılan Atamalar
-            Board.Todo.Add(gorev1);
-            Board.InProgress.Add(gorev2);
-            Board.Done.Add(gorev3);
-
-
             Operations kartIslem = new Operations();
-            kartIslem.AddCard();
-
-
+            //kartIslem.AddCard();
+            kartIslem.ShowCards();
+            kartIslem.DeleteCard();
+            kartIslem.ShowCards();
+            kartIslem.DeleteCard();
+            kartIslem.ShowCards();
+            kartIslem.DeleteCard();
+            kartIslem.ShowCards();
 
         }
     }
