259d3fd13f574de9b95b8152c8376e63dbfe49ab
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-19 04:43:06 +0300

    update: main method is now updated for consecutive operation phases

--- Project2-TodoApp/Program.cs
+++ Project2-TodoApp/Program.cs
@@ -8,32 +8,52 @@ namespace Project2_TodoApp
     {
         static void Main(string[] args)
         {
+
             Operations kartIslem = new Operations();
 
-            KisiService.AddKisi();
-            KisiService.getKisiler();
+            int islem = -1;
+            do
+            {
+                Console.WriteLine("Yapmak İstediğiniz İşlemi Seçin, LİSTEYE KİŞİ EKLEME(1), LİSTEDEN KİŞİ SİLME(2), KİŞİ LİSTESİNİ GÖSTERME(3)");
+                Console.WriteLine("YENİ KART OLUŞTURMA(4), KART SİLME(5), KART TAŞIMA(6), KARTLARI LİSTELEME(7)");
+                Console.WriteLine("ÇIKIŞ(0)");
+                islem = Convert.ToInt32(Console.ReadLine());
 
-            KisiService.RemoveKisi();
-            KisiService.getKisiler();
+                switch (islem)
+                {
+                    case 0:
+                        Console.WriteLine("Çıkış Yapılıyor. Lütfen Bir Tuşa Basın");
+                        Console.ReadKey();
+                        islem = 0;
+                        break;
+                    case 1:
+                        KisiService.AddKisi();
+                        break;
+                    case 2:
+                        KisiService.RemoveKisi();
+                        break;
+                    case 3:
+                        KisiService.getKisiler();
+                        break;
+                    case 4:
+                        kartIslem.AddCard();
+                        break;
+                    case 5:
+                        kartIslem.DeleteCard();
+                        break;
+                    case 6:
+                        kartIslem.MoveCard();
+                        break;
+                    case 7:
+                        kartIslem.ShowCards();
+                        break;
 
-            Console.WriteLine("-----------------------------------------");
+                }
 
-            kartIslem.ShowCards();
+            } while (islem != 0);
 
-            Console.WriteLine("-----------------------------------------");
-            kartIslem.AddCard();
-            kartIslem.ShowCards();
 
-            Console.WriteLine("-----------------------------------------");
-            kartIslem.DeleteCard();
-            kartIslem.ShowCards();
 
-            Console.WriteLine("-----------------------------------------");
-            kartIslem.MoveCard();
-            kartIslem.ShowCards();
-
-            Console.WriteLine("-----------------------------------------");
-            Console.ReadKey();
 
         }
     }
