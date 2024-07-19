6a5d4932c8d40ede767999454b30399462158016
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-07-19 04:32:54 +0300

    project is now fully functional

--- Project2-TodoApp/Program.cs
+++ Project2-TodoApp/Program.cs
@@ -10,13 +10,30 @@ namespace Project2_TodoApp
         {
             Operations kartIslem = new Operations();
 
+            KisiService.AddKisi();
+            KisiService.getKisiler();
+
+            KisiService.RemoveKisi();
+            KisiService.getKisiler();
+
+            Console.WriteLine("-----------------------------------------");
+
+            kartIslem.ShowCards();
+
+            Console.WriteLine("-----------------------------------------");
+            kartIslem.AddCard();
+            kartIslem.ShowCards();
+
+            Console.WriteLine("-----------------------------------------");
+            kartIslem.DeleteCard();
+            kartIslem.ShowCards();
+
             Console.WriteLine("-----------------------------------------");
             kartIslem.MoveCard();
             kartIslem.ShowCards();
 
             Console.WriteLine("-----------------------------------------");
             Console.ReadKey();
-
 
         }
     }
